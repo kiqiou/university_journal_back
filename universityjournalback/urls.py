2b5653f8b8740d7e90754bc0afd854da48d4ed0b
kiqiou <kuksare@gmail.com>
2025-05-02 15:29:14 +0300

    feat: add function add_session

--- universityjournalback/urls.py
+++ universityjournalback/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-from .views import get_attendance
+from .views import get_attendance, add_session
 
 urlpatterns = [
     path('auth/', include('authentication.urls')),
-    path('api/attendance/', get_attendance, name = 'attendance')
+    path('api/attendance/', get_attendance, name = 'attendance'),
+    path('api/add_session/', add_session, name = 'add_session'),
 ]
 
 
--- universityjournalback/views.py
+++ universityjournalback/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Attendance
-from .serializers import AttendanceSerializer
+from .models import Attendance, Course, Session
+from .serializers import AttendanceSerializer, SessionSerializer
 
 @api_view(['GET', 'POST'])
 def get_attendance(request):
@@ -13,3 +13,22 @@ def get_attendance(request):
         return Response({'error': f'Ошибка: {str(e)}'}, status=500)
     
 
+@api_view(['GET', 'POST'])
+def add_session(request):
+    try:
+        type = request.data.get('type')
+        date = request.data.get('date')
+        course_id = request.data.get('course_id')
+
+        if not course_id or not type or not date:
+            return Response({'error': 'Айди курса, тип и дата обязательны'}, status=400)
+
+        course = Course.objects.filter(id=course_id).first()
+        if not course:
+            return Response({'error': 'Некорректный ID курса'}, status=400)
+
+        session = Session(type = type, date = date, course = course)
+        session.save()
+        return Response(SessionSerializer(session).data, status=201,)
+    except Exception as e:
+        return Response({'error': f'Ошибка: {str(e)}'}, status=500)

f5a8bd3e526567812e5741fe60ae79d909595d84
kiqiou <kuksare@gmail.com>
2025-10-25 09:41:32 +0300

    fix: add faculties and courses data in migrations

--- authentication/migrations/0002_data_roles_and_admins.py
+++ authentication/migrations/0002_data_roles_and_admins.py
@@ -4,6 +4,8 @@ from django.contrib.auth.hashers import make_password
 def create_roles_and_users(apps, schema_editor):
     Role = apps.get_model('authentication', 'Role')
     User = apps.get_model('authentication', 'User')
+    Course = apps.get_model('authentication', 'Course')
+    Faculty = apps.get_model('authentication', 'Faculty'),
 
     roles_data = [
         (1, 'Преподаватель'),
@@ -14,13 +16,37 @@ def create_roles_and_users(apps, schema_editor):
     ]
     for role_id, role_name in roles_data:
         Role.objects.update_or_create(id=role_id, defaults={'role': role_name})
+    
+    courses_data = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+    ]
+    for course_id, course_name in courses_data:
+        Course.objects.update_or_create(id=course_id, name=course_name)
 
-    # Создаём двух администраторов с паролем 111111Aa_
+    faculties_data = [
+        (1, 'Экономический'),
+        (2, 'Юридический'),
+    ]
+    for faculty_id, faculty_name in faculties_data:
+        Faculty.objects.update_or_create(id=faculty_id, name=faculty_name)
+
     if not User.objects.filter(username='admin1').exists():
         User.objects.create(
             username='admin1',
             password=make_password('111111Aa_'),
             role=Role.objects.get(id=2),
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+        )
+    if not User.objects.filter(username='admin1').exists():
+        User.objects.create(
+            username='dean',
+            password=make_password('111111Aa_'),
+            role=Role.objects.get(id=4),
             is_staff=True,
             is_superuser=True,
             is_active=True,

de089a78798e28ac47fbff1f9faca1dc1642d4a2
kiqiou <kuksare@gmail.com>
2025-10-25 10:01:11 +0300

    fix: fixed data for courses and faculties

--- authentication/migrations/0002_data_roles_and_admins.py
+++ authentication/migrations/0002_data_roles_and_admins.py
@@ -18,20 +18,22 @@ def create_roles_and_users(apps, schema_editor):
         Role.objects.update_or_create(id=role_id, defaults={'role': role_name})
     
     courses_data = [
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    
     ]
     for course_id, course_name in courses_data:
-        Course.objects.update_or_create(id=course_id, name=course_name)
+        Course.objects.update_or_create(id=course_id, defaults={'name': course_name})
 
     faculties_data = [
         (1, 'Экономический'),
         (2, 'Юридический'),
     ]
+    
     for faculty_id, faculty_name in faculties_data:
-        Faculty.objects.update_or_create(id=faculty_id, name=faculty_name)
+        Faculty.objects.update_or_create(id=faculty_id, defaults={'name': faculty_name})
 
     if not User.objects.filter(username='admin1').exists():
         User.objects.create(

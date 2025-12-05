2664f30c72a6153c49c94c9f986b64d65cded029
kiqiou <kuksare@gmail.com>
2025-12-05 09:40:07 +0300

    fix: added new migration for roles

---
+++ authentication/migrations/0001a_roles.py
@@ -0,0 +1,28 @@
+from django.db import migrations
+
+def create_roles(apps, schema_editor):
+    Role = apps.get_model('authentication', 'Role')
+
+    roles = [
+        (1, "Преподаватель"),
+        (2, "Администратор 1"),
+        (3, "Администратор 2"),
+        (4, "Декан"),
+        (5, "Студент"),
+    ]
+
+    for role_id, name in roles:
+        Role.objects.update_or_create(
+            id=role_id,
+            defaults={"name": name}
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authentication', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_roles),
+    ]
